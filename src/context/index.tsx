3dfa7fe644b02624e447c737f3b60a4db01a7622
Edwilson <edwsilva@gmail.com>
2025-08-01 12:09:13 -0300

    Colocando o children para renderizar os elementos que estiver dentro da componente Provider

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, type JSX } from "react";
 
 interface IUser {
   img: string;
@@ -16,3 +16,19 @@ const AppContext = createContext<IAppContext>({
   // Passando o valor inicial do contexto
   user: undefined,
 });
+
+// Criar o provider para disponibilizar para todo o APP
+const AppProvider = ({ children }: { children: JSX.Element }) => {
+  // O contexto criado será exportado como um provider
+  // vslur=={{será distribuído no APP}}
+  return (
+    <AppContext.Provider value={{ user: undefined }}>
+      {/* Ele é utilizado para renderizar tudo que estiver
+      dentro do privider */}
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Agora preciso exportar  as constantes
+export { AppContext, AppProvider };
