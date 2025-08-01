a71e6594558f2d85ed4ba5442a5370f3fdee3eab
Edwilson <edwsilva@gmail.com>
2025-08-01 12:31:15 -0300

    Função para simular uma requisição a um servidor real

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import { useState } from "react";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
+  async function getData() {
+    //Simular um delay de 2 segundos em um request
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return {
+      img: "https://media.istockphoto.com/id/172735497/pt/foto/senior-cidad%C3%A3o.webp?a=1&s=612x612&w=0&k=20&c=FBEcUyMy4ltWZUIvLcaHbepZHdngz2S1f3XNjd1O6Cg=",
+      name: "Edwilson da Silva",
+    };
+  }
   return <HomePage />;
 }
 
