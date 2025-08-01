840525877a18904d6461684e35455074f14cdbe0
Edwilson <edwsilva@gmail.com>
2025-08-01 12:36:22 -0300

    Pegando o contexto para utilizar no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //https://www.youtube.com/watch?v=hYNjpbXe7V0&list=PLgM_vQNu_4CBV5D1uLH1gzvZ3lNYWujC3?
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import { HomePage } from "./pages/HomePage";
 
